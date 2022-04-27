48e6291adfd5b9440ff371e28ceba614d4369683
Christian Sansovich <csans@Christians-MacBook-Pro-2.local>
2022-04-26 22:43:56 -0400

    added nextjs

---
+++ pages/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Registration from './Registration';
+
+function App() {
+  return (
+    <div className="App">
+      <Registration />
+    </div>
+  )
+}
+
+export default App
---
+++ pages/Registration.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Registration() {
+  return (
+    <h1>Hola, senior</h1>
+  )
+}
+
+export default Registration
