e70cf6f359d09ded736e6695dee8966e9da10a06
Christian Sansovich <csans@Christians-MacBook-Pro-2.local>
2022-04-28 17:37:13 -0400

    login working w database

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,6 @@
-// import React from 'react';
-// import { createRoot } from 'react-dom/client';
-// import App from './App'
-
-// const root = createRoot(document.querySelector('#root')!);
-
-// root.render(
-//     <App />
-// )
 
 import Head from 'next/head'
-import Menu from '../components/menu'
+import Menu from '../components/splash-menu'
 import Footer from '../components/footer'
 import Body from '../components/body'
 
