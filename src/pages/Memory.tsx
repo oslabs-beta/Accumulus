765d616e53e49aec4608aa42637320dd966c5760
Christian Sansovich <csans@Christians-MacBook-Pro-2.local>
2022-05-12 12:21:23 -0400

    merge

--- src/pages/Memory.tsx
+++ src/pages/Memory.tsx
@@ -3,11 +3,11 @@ import StackedBarFnGraph from '../components/StackedBarFnGraph';
 import AlloFunctionSelector from '../components/AlloFunctionSelector';
 import MemReduction from '../components/MemReduction';
 import Sidebar from '../components/Sidebar';
-import {
-  DashSideBar,
-  DashboardGrid,
-  EvenDashGraphBox,
-  GraphContainered,
+import { DashSideBar, 
+  DashboardGrid, 
+  EvenDashGraphBox, 
+  GraphContainer, 
+  GraphContainered, 
   Header,
   Selector,
   ResourcesGrid,
