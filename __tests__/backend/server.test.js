cbfe918ad0a485ab3355ff3879e30c039595fdc0
Mark Yencheske <markyencheske@gmail.com>
2022-05-18 15:41:57 -0500

    testing has begun

---
+++ __tests__/backend/server.test.js
@@ -0,0 +1,20 @@
+// import 'regenerator-runtime/runtime';
+
+const request = require('supertest');
+const fs = require('fs');
+const path = require('path');
+
+const server = 'http://localhost:3000'
+
+describe('Route integration', () => {
+  describe('/api/user/login', () => {
+    describe('GET', () => {
+      it ('responds with 200 status and json content type', async () => {
+        return request(server)
+          .get('/api/user/login')
+          .expect('Content-Type', /json/)
+          .expect(200)
+      })
+    })
+  })
+})
